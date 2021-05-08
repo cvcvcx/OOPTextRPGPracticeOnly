6308f9fcd92880294a0df8bc7529f4b437f2fc92
CHOChangHun <cvcvcx9@gmail.com>
2021-05-08 12:02:34 +0900

    Creature

--- ConsoleApp1/Creature.cs
+++ ConsoleApp1/Creature.cs
@@ -8,5 +8,24 @@ namespace ConsoleApp1
 {
     class Creature
     {
+        protected int _hp = 0;
+        protected int _attack = 0;
+        
+        public int GetHp() { return _hp; }
+        public int GetAttack() { return _attack; }
+
+        public void SetInfo(int hp, int attack)
+        {
+            _hp = hp;
+            _attack = attack;
+        }
+
+        public bool IsDead() { return _hp <= 0; }
+        public void OnDamege(int damage) 
+        {
+            _hp -= damage;
+            if (_hp < 0)
+                _hp = 0;
+        }
     }
 }

651a730e1ec3fced23e8669895d63c02751fecda
CHOChangHun <cvcvcx9@gmail.com>
2021-05-08 12:10:10 +0900

    Process함수 시작 001

--- ConsoleApp1/Game.cs
+++ ConsoleApp1/Game.cs
@@ -6,7 +6,37 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    enum GameMode
+    {
+        None,
+        Lobby,
+        Town,
+        Field
+    }
     class Game
     {
+        public void Process()
+        {
+            GameMode mode = GameMode.Lobby;
+            Console.WriteLine("게임에 접속하였습니다.");
+            Console.WriteLine("캐릭터를 생성해주세요.");
+            
+            switch (mode)
+            {
+                case GameMode.Lobby:
+                    ProcessLobby();
+                    break;
+                case GameMode.Town:
+
+                    break;
+                case GameMode.Field:
+
+                    break;
+            }
+        }
+        private void ProcessLobby()
+        {
+
+        }
     }
 }
--- ConsoleApp1/Monster.cs
+++ ConsoleApp1/Monster.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    class Monster
+    class Monster : Creature
     {
     }
 }

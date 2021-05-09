3bb80016c9f63b5cf04e800125ffc1c615a1a348
CHOChangHun <cvcvcx9@gmail.com>
2021-05-09 14:38:08 +0900

    OOP6 First git

---
+++ OOP6/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP6
+{enum GameMode
+    {
+        None,
+        Lobby,
+        Town,
+        Field
+    }
+
+    class Game
+    {
+        public void Process()
+        {
+
+        }
+
+    }
+}

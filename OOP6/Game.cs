5e9bbc12c5805c31b19ca6045daa0b1141b0c164
CHOChangHun <cvcvcx9@gmail.com>
2021-05-09 14:43:26 +0900

    게임클래스 작성중

--- OOP6/Game.cs
+++ OOP6/Game.cs
@@ -15,10 +15,37 @@ namespace OOP6
 
     class Game
     {
+        Player player = null;
+        Monster monster = null;
+        protected GameMode mode = GameMode.Lobby;
         public void Process()
+        {
+            switch (mode)
+            {
+                case GameMode.Lobby:
+                    ProcessLobby();
+                    break;
+                case GameMode.Town:
+                    ProcessTown();
+                    break;
+                case GameMode.Field:
+                    ProcessField();
+                    break;
+            }
+
+        }
+        private void ProcessLobby()
+        {
+            Console.WriteLine("게임에 접속하였습니다.");
+        }
+        private void ProcessTown()
         {
 
         }
+        private void ProcessField()
+        {
 
+        }
     }
+   
 }

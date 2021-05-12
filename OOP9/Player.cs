c8c25d79e5177118ecfc889fc367c21996af1aba
CHOChangHun <cvcvcx9@gmail.com>
2021-05-12 14:54:05 +0900

    Player 클래스 작성 완료

--- OOP9/Player.cs
+++ OOP9/Player.cs
@@ -6,7 +6,39 @@ using System.Threading.Tasks;
 
 namespace OOP9
 {
-    class Player
+    enum PlayerType
     {
+        None,
+        Knight,
+        Archer,
+        Mage
+    }
+    class Player:Creature
+    {
+        PlayerType _playerType = PlayerType.None;
+        protected Player(PlayerType type) : base(CreatureType.Player){ _playerType = type; }
+        public PlayerType GetPlayerType() { return _playerType; }
+    }
+
+    class Knight : Player
+    {
+        public Knight() : base(PlayerType.Knight) 
+        {
+            SetInfo(100, 10);
+        }
+    }
+    class Archer : Player
+    {
+        public Archer() : base(PlayerType.Archer)
+        {
+            SetInfo(75, 12);
+        }
+    }
+    class Mage : Player
+    {
+        public Mage() : base(PlayerType.Mage)
+        {
+            SetInfo(50, 15);
+        }
     }
 }
